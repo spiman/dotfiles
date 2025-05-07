887c4a8326d12340422c0c6f92bc74e778bdee48
Spyros Mandekis <s.mandekis@gmail.com>
2025-05-07 16:50:27 +0300

    switching laptop

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,8 +2,10 @@
 #set -x PATH "/usr/local/opt/grep/libexec/gnubin" $PATH
 #set -x MANPATH "/usr/local/opt/coreutils/libexec/gnuman" $PATH
 set -x PATH $PATH "$HOME/.git-radar"
-set -x PATH $PATH "$HOME/.pyenv/bin"
-#set -x PATH $PATH "$HOME/.jenv"
+set -x PATH $PATH "$HOME/.rbenv/bin"
+set -x PATH $PATH "$HOME/go/bin" "/usr/local/go/bin"
+set -x PATH $PATH "$HOME/.local/bin"
+set -x PATH $PATH "/snap/bin"
 set -x EDITOR "/usr/bin/vim"
 
 #fzf
@@ -13,32 +15,20 @@ set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 #rg
 set -x RIPGREP_CONFIG_PATH "$HOME/.rgrc"
 
-set -x GOPATH "$HOME/go"
-
 alias ls="ls --color=always"
 alias grep="grep --color=always"
 alias egrep="egrep --color=always"
 alias k="kubectl"
-alias kubectx="kubectl config current-context"
 
 abbr --add fuckit "git add . --all; git commit -m '.'"
 
 status --is-interactive; and source (rbenv init -|psub)
-#status --is-interactive; and source (jenv init -| psub)
 
-# i hate this piece of shit language
-set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
-
-status --is-interactive; and source (pyenv init -| psub)
-
-# Plum GPG
-
-set -x GPG_TTY (tty)
-set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-gpgconf --launch gpg-agent
-
-# End GPG Block
+# fucking python
+set -Ux PYENV_ROOT $HOME/.pyenv
+set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
 
 function fish_prompt
   if set -q VIRTUAL_ENV
@@ -51,6 +41,7 @@ function fish_prompt
   echo -n (prompt_pwd)
   set_color normal
   echo (git-radar --fish --fetch)
+  set_color normal
   echo -n '# '
 end
 
@@ -63,7 +54,7 @@ function sudo
 end
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
-eval (direnv hook fish)
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/opt/google-cloud-sdk/path.fish.inc' ]; . '/opt/google-cloud-sdk/path.fish.inc'; end
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
